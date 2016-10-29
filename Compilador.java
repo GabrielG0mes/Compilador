f0736a42515ef09c08642e40c024206688abfd7b
GabrielG0mes <gabriel_sousa_94@hotmail.com>
2016-10-29 20:40:01 -0200

    Create Compilador.java

---
+++ Compilador.java
@@ -0,0 +1,14 @@
+
+/**
+ *
+ * @autores Gabriel de Sousa Gomes / Nayara Gomes de Oliveira Pereira
+ */
+public class Compilador {
+    
+    public static void main(String[] args) {
+        Interface janela = new Interface();
+        janela.setLocationRelativeTo(null);
+        janela.show();
+    }
+    
+}
