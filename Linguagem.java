424ec95ba5f63ce172f564f1195403a083fff6dd
Naolih <nr.tearsdoll@gmail.com>
2016-10-31 11:24:41 -0200

    Create Linguagem.java

---
+++ Linguagem.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+
+
+public class Linguagem {
+
+    ArrayList tokens = new ArrayList();
+    
+    String identificador = "#[A-Z|a-z|0-9]+";
+    String numero = "[0-9]+";
+
+    String soma = "M415";
+    String subtrai = "M3N05";
+    String multiplica = "V3Z35";
+    String divide = "D1V1D1D0";
+    String atribui = "<-";
+
+    
+    String abreparentese ="[(]";
+    String fechaparentese = "[)]";
+
+    String escreva = "35CR3V4";
+
+    String inicio = "1N1C10";
+    String fim = "F1M";
+
+    String padroes[] = {identificador, numero, soma, subtrai, multiplica, divide,
+        atribui, abreparentese, fechaparentese, escreva, inicio, fim};
+
+    public ArrayList getTokens() {
+        return tokens;
+    }
+
+    public String nomePadrao(int index) {
+        switch(index) {
+            case 0: 
+                return "identificador";
+            case 1:
+                return "número";
+            case 2:
+                return "operador";
+            case 3:
+                return "operador";
+            case 4:
+                return "operador";
+            case 5:
+                return "operador";
+            case 6:
+                return "atribuição";
+            case 7:
+                return "separador";
+            case 8:
+                return "separador";
+            case 9:
+                return "palavra reservada";
+            case 10:
+                return "palavra reservada";
+            case 11:
+                return "palavra reservada";
+            default:
+                return "Erro léxico";
+        }
+    }
+}
